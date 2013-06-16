810033420cb7eb9dcca596ccd044ef8d7cf53f36
maduxing@gmail.com <maduxing@gmail.com@231c32d3-2472-0ff0-6861-9f03758c9029>
2013-06-16 06:01:59 +0000

    梳理异步拉取的评论

--- background.js
+++ background.js
@@ -6,7 +6,7 @@
 电邮：IamSigma.js@gmail.com
 反馈：http://rrurl.cn/hM9mhk
 版本：1.3.2
-更新：2013年5月20日 00:47:48
+更新：2013年6月16日 14:01:19
 */
 var isOpen = true ,
 	DELAY_POPICON = 2000,
@@ -57,15 +57,17 @@ var asynchReqSet = [
 	  		reg:/^http:\/\/status\.renren\.com\/feedcommentretrieve\.do///
 	  	},
 	  	{
-	  		title:'首页-展开评论2',
-	  		//http://page.renren.com/600002874/album/replyList 主页相册
-	  		//http://page.renren.com/699153758/note/replyList 主页日志
-			//http://page.renren.com/doing/replyList 主页状态-展开的异步评论
-	  		reg:/^http:\/\/page\.renren\.com\/(\d{9}\/(album|note)|doing)\/replyList///
+	  		title:'首页-新鲜事评论',
+	  		//http://page.renren.com/600002874/album/replyList 相册
+	  		//http://page.renren.com/699153758/note/replyList 日志
+			//http://page.renren.com/600002570/photo/replyList 公共主页-图片
+			//http://page.renren.com/699139427/share/replyList
+			//http://page.renren.com/doing/replyList 公共主页-状态
+	  		reg:/^http:\/\/page\.renren\.com\/((\d{9}\/(album|note|photo|share))|doing)\/replyList/
 	  	},
 	  	{
-	  		title:'首页-新鲜事',
-	  		reg:/http:\/\/www\.renren\.com\/feedretrieve2\.do/
+	  		title:'首页-更多新鲜事',
+	  		reg:/http:\/\/www\.renren\.com\/feedretrieve\d\.do/
 	  	},
 	  	{
 	  		title:'小站',//“较早的评论”
