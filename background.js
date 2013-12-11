d3d3b01621c8ef4ad2a4ce03743c1816f4b22b2f
maduxing@gmail.com <maduxing@gmail.com@231c32d3-2472-0ff0-6861-9f03758c9029>
2013-12-11 03:08:35 +0000

    a new start

--- background.js
+++ background.js
@@ -5,19 +5,20 @@
 微信：rrsbfilter
 电邮：IamSigma.js@gmail.com
 反馈：http://rrurl.cn/hM9mhk
-版本：1.3.1
-更新：2013年4月16日 16:54:50
+版本：1.3.3
+更新：2013年12月8日 03:02:37
 */
 var isOpen = true ,
 	DELAY_POPICON = 2000,
 	DELAY_KILLEDICON = 5000,
+	scanCount = 0,//检测总数
 	SBNumCurPage = 0;
 
 chrome.extension.onRequest.addListener(
 	function(request, sender, sendResponse) {
 		switch( request.action ){
 			case 'getIsOpen':
-				sendResponse({isOpen: isOpen});
+				sendResponse({'isOpen': isOpen});
 				break;
 			case 'setKilledNum':
 				if( parseInt(request.killedNum) > 0 ){
@@ -27,10 +28,6 @@ chrome.extension.onRequest.addListener(
 					setBadge( request.killedNum + 'sb' ,  [0 , 255, 0, 100] );
 					removeBadge( DELAY_KILLEDICON );
 				}
-				SBNumCurPage += parseInt(request.killedNum);//累加
-				break;
-			case 'getSBNumCurPage':
-				sendResponse({SBNumCurPage: SBNumCurPage});
 				break;
 			default:break;
 		}
@@ -40,95 +37,107 @@ chrome.extension.onRequest.addListener(
 chrome.browserAction.onClicked.addListener(function(tabs) {
 	switchIcon();
 	isOpen = !isOpen ;
-	isOpen ? console.log('\u4EBA\u4EBA2B\u8FC7\u6EE4\u5668OPEN -' + getTime()) : console.log('\u4EBA\u4EBA2B\u8FC7\u6EE4\u5668CLOSE-' + getTime());
 	chrome.tabs.executeScript(null, {file:"killer.js"});
 });
 
 //需要处理的异步请求集合
 var asynchReqSet = [
-  		{
-  			title:'相册',
-	  		//http://photo.renren.com/photo/105663055/photo-5733354611/ajax
-	  		reg:/^http:\/\/photo\.renren\.com\/photo\/\d{9}\/.+\/ajax///
-	  	},
-	  	{
-	  		title:'首页-展开评论1',//个人状态-展开评论
-	  		//http://status.renren.com/feedcommentretrieve.do
-	  		reg:/^http:\/\/status\.renren\.com\/feedcommentretrieve\.do///
-	  	},
-	  	{
-	  		title:'首页-展开评论2',
-	  		//http://page.renren.com/600002874/album/replyList 主页相册
-	  		//http://page.renren.com/699153758/note/replyList 主页日志
-			//http://page.renren.com/doing/replyList 主页状态-展开的异步评论
-	  		reg:/^http:\/\/page\.renren\.com\/(\d{9}\/(album|note)|doing)\/replyList///
-	  	},
-	  	{
-	  		title:'首页-新鲜事',
-	  		reg:/http:\/\/www\.renren\.com\/feedretrieve2\.do/
-	  	},
-	  	{
-	  		title:'小站',//“较早的评论”
-	  		reg:/http:\/\/zhan\.renren\.com\/.*\/\d+\/comment\/list/
-	  	},
-	  	{
-	  		title:'我的分享-评论',
-	  		reg:/http:\/\/share\.renren\.com\/share\/showcomment\.do/
-	  	},
-	  	{
-	  		title:'他人的分享-评论',
-	  		reg:/http:\/\/share\.renren\.com\/share\/getmorecomment\.do/
-	  	},
-	  	{
-	  		title:'他人的视频分享-显示较早之前的评论',
-	  		reg:/http:\/\/share\.renren\.com\/share\/comment\/moreurlcomment/
-	  	},
-	  	{
-	  		title:'我的分享-列表-异步评论',
-	  		reg:/http:\/\/share\.renren\.com\/share\/comment\/getcomments/
-	  	}
-	  	,{//http://gossip.renren.com/ajaxgossiplist.do
-	  		title:'留言板翻页',
-	  		reg:/http:\/\/gossip\.renren\.com\/ajaxgossiplist\.do/
-	  	}
-	  	,{
-	  		title:'公共主页-相册',
-	  		reg:/http:\/\/page\.renren\.com\/ajaxcomment\/list/
-	  	}
+	{
+		title:'相册',
+		//http://photo.renren.com/photo/105663055/photo-5733354611/ajax
+		reg:/^http:\/\/photo\.renren\.com\/photo\/\d{9}\/.+\/ajax///
+	},
+	{
+		title:'首页-日志评论',
+		//http://status.renren.com/feedcommentretrieve.do
+		reg:/^http:\/\/status\.renren\.com\/feedcommentretrieve\.do///
+	},
+	{
+		title:'首页-新鲜事评论',
+		//http://page.renren.com/600002874/album/replyList 相册
+		//http://page.renren.com/699153758/note/replyList 日志
+		//http://page.renren.com/600002570/photo/replyList 公共主页-图片
+		//http://page.renren.com/699139427/share/replyList
+		//http://page.renren.com/doing/replyList 公共主页-状态
+		reg:/^http:\/\/page\.renren\.com\/((\d{9}\/(album|note|photo|share))|doing)\/replyList/
+	},
+	{
+		title:'首页-更多新鲜事',
+		reg:/http:\/\/www\.renren\.com\/feedretrieve\d\.do/
+	},
+	{
+		title:'小站',//“较早的评论”
+		reg:/http:\/\/zhan\.renren\.com\/.*\/\d+\/comment\/list/
+	},
+	{
+		title:'我的分享-评论',
+		reg:/http:\/\/share\.renren\.com\/share\/showcomment\.do/
+	},
+	{
+		title:'好友的分享-评论',
+		reg:/http:\/\/share\.renren\.com\/share\/getmorecomment\.do/
+	},
+	{
+		title:'好友的视频分享-显示较早之前的评论',
+		reg:/http:\/\/share\.renren\.com\/share\/comment\/moreurlcomment/
+	},
+	{
+		title:'好友的最新相册',
+		reg:/http:\/\/photo\.renren\.com\/photo\/\d{9}\/photo\-\d+\/ajaxmsy/
+	},
+	{//added by 1.3.3
+		title:'首页-好友-图片动态-还有N条回复',
+		reg:/http:\/\/photo\.renren\.com\/photo\/feedcommentretrieve\.do/
+	},
+	{
+		title:'我的分享-列表-异步评论',
+		reg:/http:\/\/share\.renren\.com\/share\/comment\/getcomments/
+	}
+	,{
+		title:'留言板-好友',
+		reg:/http:\/\/gossip\.renren\.com\/ajaxgossiplist\.do/
+	}
+	,{
+		title:'留言板-个人',
+		reg:/http:\/\/gossip\.renren\.com\/getconversation\.do/
+	}
+	,{
+		title:'公共主页-相册',
+		reg:/http:\/\/page\.renren\.com\/ajaxcomment\/list/
+	}
+	,{
+		title:'公共主页-相册2',
+		reg:/http:\/\/page\.renren\.com\/ajaxgetphoto\.do/
+	}
+	,{
+		title:'公共主页-相册2-翻页',
+		reg:/http:\/\/page\.renren\.com\/ajaxgetphotocomment/
+	}
 ];
 
 //异步加载
-chrome.webRequest.onCompleted.addListener(
-  function(details){
+var executeScriptTimer;
+chrome.webRequest.onCompleted.addListener(function(details){
   	var url = details.url;
   	for( var i = 0 , len = asynchReqSet.length; i < len ; i++ ){
   		if( asynchReqSet[i].reg.test( url ) ){
-  			console.log('synch-title:' + asynchReqSet[i].title + '-' + url );//调试信息
-		  	chrome.tabs.executeScript(null, {file:"killer.js"});
+  			clearTimeout( executeScriptTimer );
+		  	executeScriptTimer = setTimeout(function(){
+		  		chrome.tabs.executeScript(null, {file:"killer.js","runAt":"document_end"});
+	  			console.log( asynchReqSet[i].title , url );//调试信息
+	  		},1e3 );
 		  	break;
   		}
   	}
-  }, {urls: ["http://*.renren.com/*"]}
-);
-
-
-function getTime(){
-	var str = '',
-		d = new Date(),
-		h = d.getHours(),
-		m = d.getMinutes(),
-		s = d.getSeconds();
-	str = h + ':' + ( m < 10 ? '0' + m : m ) + ':' + ( s < 10 ? '0' + s : s );
-	return str;
-}
+},
+{urls: ["http://*.renren.com/*"]});
 
 function setBadge( text , color ){
 	chrome.browserAction.setBadgeText({"text": text || ''});
 	color && chrome.browserAction.setBadgeBackgroundColor({"color": color });
 }
 
-function removeBadge( delay ){
-	//移除标记
+function removeBadge( delay ){//移除标记
 	clearTimeout( window["t"] );
 	window["t"] = setTimeout(function(){
 		setBadge();
--- killer.js
+++ killer.js
@@ -5,65 +5,92 @@
 微信：rrsbfilter
 电邮：IamSigma.js@gmail.com
 反馈：http://rrurl.cn/hM9mhk
-版本：1.3.1
-更新：2013年4月16日 14:52:39
+版本：1.3.3
+更新：2013年12月8日 03:02:32
 */
+var VERSION = '1.3.3';
 var isOpen = true;
-//词库目前不够智能，向误伤的孩纸表示哀怜，后续会更新成动态词库 -Sigma
 var SBWORD = [
 '互访', '求来访', '求关注', '求人气', '求围观', '求访', '访必回','互粉',
 '关注我','互踩','刷人气','加好友','加我好友','求交往','求交友','请关注',
-'速进','联系我','见状态','回访','欢迎来访',
-'来看看吧','瘦身', '减肥','减重','淘宝兼职'
+'速进','联系我','见状态','回访','欢迎来访','加微信',
+'来看看吧','瘦身', '减肥','减重','淘宝兼职','淘宝店',
+'taobao\.com'
 ];
 //堆积的高频无意义纯字符
-var punctuations = ['\\.','·','。',',','，','`','…','0','1','2','3','='];
+var punctuations = ['\\.','·','。',',','，','`','…','~','0','1','2','3','='];
+var selectorSet = {
+	'page':{item:'div.replies .p-reply',cmt:'p.text'},//公共主页
+	'main':{item:'.feed-replies .a-reply',cmt:'p.text'},//首页
+	'gossip':{item:'#talk .cmt-body',cmt:'div.text-content'},
+	'blog':{item:'div.statuscmtitem.clearfix',cmt:'span.replycontent'},
+	'status':{item:'div.statuscmtlist div.statuscmtitem:not([class~="reply-adding"])',cmt:'span.replycontent'}
+}
 var pageSet = [
 	{
 		title:'我的分享列表',//首页匹配范围的子集，所以置于首页前1.3.1
 		reg:/http:\/\/www\.renren\.com\/\d{9}#!?\/\/share\/share\/\d{9}/,
 		selector:{item:'.usertalk .replybody',cmt:'span.replycontent'}
 	}
-	,{//1.3.1 http://www.renren.com/103433276#//status/status?id=103433276
+	,{
 		title:'个人状态',//所有状态(未解决)
 		reg:/http:\/\/www\.renren\.com\/\d{9}#\/\/status\/status/,
-		// selector:{item:'div.nomore div.statuscmtitem',cmt:'span.replycontent'}
-		selector:{item:'div.statuscmtlist div.statuscmtitem',cmt:'span.replycontent'}
+		selector:selectorSet['status']
+	}
+	,{//1.3.3
+		title:'与我相关-我参与的',
+		reg:/http:\/\/www\.renren\.com\/\d{9}#!\/\/matter/,
+		selector:selectorSet['status']
 	}
 	,{//1.3.1
 		title:'首页',
 		reg:/http:\/\/www\.renren\.com\/\d{9}(((\?.*)?$)|((#.*)$))/,
-		selector:{item:'.feed-replies .a-reply',cmt:'p.text'}
+		selector:selectorSet['main']
 	}
 	,{
 		title:'个人主页',
 		reg:/http:\/\/www\.renren\.com\/\d{9}\/profile/,
-		selector:{item:'.feed-replies .a-reply',cmt:'p.text'}
+		selector:selectorSet['main']
 	}
 	,{
-		title:'个人日志',//验证页面http://blog.renren.com/blog/103433276/813554295
+		title:'个人日志',
 		reg:/http:\/\/blog\.renren\.com\/blog\/\d{9}\/.+/,
-		selector:{item:'div.statuscmtitem.clearfix',cmt:'span.replycontent'}//2013.04.16
+		selector:selectorSet['blog']
 	}
 	,{
-		title:'留言板',
+		title:'留言板-好友',
 		reg:/http:\/\/gossip\.renren\.com\/getgossiplist\.do\?id=\d{9}/,
-		selector:{item:'#talk .cmt-body',cmt:'div.text-content'}
+		selector:selectorSet['gossip']
 	}
-	,{//1.3.1 http://page.renren.com/699153758/note/813996479
+	,{
+		title:'留言板-个人',
+		reg:/http:\/\/gossip\.renren\.com\/.*/,
+		selector:selectorSet['gossip']
+	}
+	,{//1.3.1
 		title:'公共主页日志',//公共主页首页的子集，前置
 		reg:/http:\/\/page\.renren\.com\/.+\/note\/\d+/,
 		selector:{item:'#commentlist li',cmt:'div.text-content'}
 	}
+	,{//1.3.3
+		title:'公共主页日志2',//公共主页首页的子集，前置
+		reg:/http:\/\/page\.renren\.com\/\d{9}\/channel\-noteshow/,
+		selector:selectorSet['page']
+	}
 	,{//1.3.1
 		title:'公共主页相册',
 		reg:/http:\/\/page\.renren\.com\/\d{9}\/channel\-photoshow/,
-		selector:{item:'div.replies div.p-reply.clearfix',cmt:'p.text'}
+		selector:selectorSet['page']
+	}
+	,{//1.3.3
+		title:'公共主页相册2',
+		reg:/http:\/\/page\.renren\.com\/\d{9}\/photo\/\d+/,
+		selector:{item:'dl.replies dd',cmt:'p.content'}
 	}
 	,{//1.3.1
-		title:'公共主页首页',//http://page.renren.com/601257806?checked=true
+		title:'公共主页首页',
 		reg:/http:\/\/page\.renren\.com\/\d{9}/,
-		selector:{item:'.feed-replies .a-reply',cmt:'p.text'}//同个人主页
+		selector:selectorSet['main']
 	}
 	,{//1.3.1
 		title:'小组',
@@ -78,91 +105,95 @@ var pageSet = [
 	,{//1.3.1
 		title:'他人分享',
 		reg:/http:\/\/share\.renren\.com\/share\/\d{9}/,
-		selector:{item:'div.statuscmtitem.clearfix',cmt:'span.replycontent'}
+		selector:selectorSet['blog']
 	}
 	,{//1.3.1
 		title:'日志分享',
 		reg:/http:\/\/blog\.renren\.com\/share\/\d{9}/,
 		selector:{item:'#cmtsListCon>div.replies div.statuscmtitem.clearfix',cmt:'span.replycontent'}
 	}
-	,{//1.3.1
+	,{
 		title:'个人相册',//图片列表、图片详情
 		reg:/http:\/\/photo\.renren\.com\/photo\/\d{9}/,
-		selector:{item:'div.replies dl.replies dd',cmt:'p.content'}
+		selector:{item:'div.replies dl.replies dd:not([class^="digger"])' ,cmt:'p.content'}
+	}
+	,{
+		title:'好友最新相册',
+		reg:/http:\/\/photo\.renren\.com\/\d{9}#!\/photo\/\d{9}\/album\/friends/,
+		selector:{item:'ul.photoViewerCommentsList li' ,cmt:'div'}
 	}
 ];
 
-chrome.extension.sendRequest({
-		action: "getIsOpen"
-	},
-	function(response) {
-		isOpen = response.isOpen;
-		if (isOpen) {
-			killer();
-		}
-	}
-);
+chrome.extension.sendRequest({action: "getIsOpen"},function(response) {
+	response.isOpen && killer();
+});
 
 function killer(){
 	var curURL = window.location.href.toLowerCase();
-
 	for( var i = 0 ,l = pageSet.length; i < l ; i ++){
 		var curPage = pageSet[i];
 		if( curPage.reg.test(curURL)){
-			_log( curPage.title );
-			superKiller( curPage.selector );
+			superKiller( curPage );
 			break;
 		}
 	}
 }
-function setKilledNum(n) {//设置回显个数
+
+function setKilledNum( killedNum , scanCount ) {//设置回显个数
+	var body = document.querySelector('body');
+	body.setAttribute('rrsb_scancount', +(body.getAttribute('rrsb_scancount')||0) + scanCount );
+	body.setAttribute('rrsb_killednum', +(body.getAttribute('rrsb_killednum')||0) + killedNum );
+
 	chrome.extension.sendRequest({
 		action: "setKilledNum",
-		killedNum: n + ''
+		killedNum: killedNum + ''
 	});
 }
-/*
-{
+/*{
 	item:containerId,容器id和item标签名，如'#comment_list dd'
 	cmt:commentId  评论内容标签名，如'p.content span'
-}
-*/
+}*/
 function superKiller( json ){
 	try{
-		var itemSelector = json.item,
-			cmtSelector = json.cmt;
-		var items = document.querySelectorAll( itemSelector );
+		var curPage = json.selector,
+			pageTitle = json.title,
+			itemSelector = curPage.item,
+			cmtSelector = curPage.cmt;
+		var items = document.querySelectorAll( itemSelector + ':not([rrsb])' );
 		var len = items.length,
-			len_curScan = 0,//本次扫描个数
+			curScanCount = 0,//本次扫描个数
 			killedNum = 0;//本次sb
-		if( len > 0 ){
+		if( len ){
 			for( var i = 0 ; i < len ; i ++ ){
 				var item = items[i];
-				if ( item.getAttribute("rrsb") != 1 ) {
-					// var	cmt = item.querySelector( cmtSelector ).innerHTML;
-					var	cmt = item.querySelector( cmtSelector ).innerText;
-					// console.log( cmt );//调试打印
-					if( filter( cmt ) ){
+				var cmtNode = item.querySelector( cmtSelector );
+				if( cmtNode ){
+					var	cmt = cmtNode.innerText;
+					if( cmt && filter( cmt ) ){
 						howToTreatSB( item , i , cmt );
 						killedNum ++;
 					}
-					item.setAttribute("rrsb",1);//标记处理
-					len_curScan++;
+					item.setAttribute("rrsb",1);
+					curScanCount++;
 				}
-			}			
-			setKilledNum( killedNum );
-			loger( len , killedNum, len_curScan );//日志输出
-		}/*else{
-			var url = encodeURIComponent( window.location.href );
-			report( 'url=' + url + '&error=' + 'noDataWasFound' );
-		}*/
+			}
+			setKilledNum( killedNum , curScanCount );
+			loger( killedNum, curScanCount , pageTitle );
+		}else{
+			dataCollector({
+				page: pageTitle,
+				total: curScanCount,
+				sb: killedNum,
+				msg:''
+			});
+		}
 	}catch( e ){
 		var url = encodeURIComponent( window.location.href ),
 			error =  encodeURIComponent( e.toString() );
-			if( !+localStorage.getItem( url ) ){
-				localStorage.setItem( url , 1 );
-				report( 'url=' + url + '&error=' + error );
-			}
+		if( !+localStorage.getItem( url ) ){
+			localStorage.setItem( url , 1 );
+			report( 'url=' + url + '&error=' + error );
+		}
 	}
 }
 
@@ -170,8 +201,32 @@ function report( msg ){
 	new Image().src = "http://xuediannao.sinaapp.com/chrome/rrsbfilter/infocenter.php?istest=0&" + msg;
 }
 
+/**
+ *处理结果收集，用于功能过期预警
+ *added 2013.05.19 ver1.3.2
+ */
+function dataCollector( json ){
+	if( localStorage ){
+		var prefix = getDay(),
+		itemName   = prefix + '_' + window.location.href ;
+		if( !+localStorage.getItem( itemName ) ){
+			json.uid  = getId();
+			if( json.uid ){
+				json.url  = window.location.href;
+				json.ver  = VERSION;
+				var paras = [];
+				for ( var i in json ){
+					paras.push( i + '=' + encodeURIComponent( json[i] ) );
+				}
+				report( paras.join('&') );
+			}
+			localStorage.setItem( itemName , 1 );
+		}
+	}
+}
+
 function filter ( cmt ) {
-	var _cmt = trim (cmt),
+	var _cmt = cmt.trim(),
 		comm = getChinese( _cmt );
 	if (comm == '') {//没有中文
 		return punctuationFilter( _cmt );
@@ -185,49 +240,49 @@ function filter ( cmt ) {
 }
 
 function punctuationFilter( cmt ){//过滤符号堆积的评论
-		var comm = filtChinese( cmt );
-		if( comm == '' ){
-			return false;
-		}
-		for (var i = 0 ; i < punctuations.length; i++) {
-			var reg = new RegExp( punctuations[i] + "+");
-			if(comm.replace( reg , '') == ''){
-				return true;
-			}
-		}
+	var comm = filtChinese( cmt );
+	if( comm == '' ){
 		return false;
+	}
+	for (var i = 0 ; i < punctuations.length; i++) {
+		var reg = new RegExp( punctuations[i] + "+");
+		if(comm.replace( reg , '') == ''){
+			return true;
+		}
+	}
+	return false;
 }
+
+var SBCmt = [];
 
 function howToTreatSB( SBNode , i , cmt ){
 	var n = 30 ,
-		tBlur  = setInterval(function(){
+	tBlur  = setInterval(function(){
 		n--;
 		SBNode.style.opacity = (n/100)+'';
 		if(n==6){
 			clearInterval( tBlur );
 		}
 	},400);
-	_log( i + 'sb:' + trim(cmt));
+	SBCmt.push( { '屁':cmt.trim() } );
 }
 
-function loger ( len , killedNum, len_curScan ) {
-	chrome.extension.sendRequest({
-		action: "getSBNumCurPage"
-	},function( response ){
-		var SBNumCurPage = response.SBNumCurPage;
-		console.log('【人人2B过滤器\'s log】');
-		console.log('当前地址：' + window.location.href);
-		// console.log('扫描个数：' + len);
-		console.log('本页 SB ：' + SBNumCurPage + '/' + len );
-		console.log('本次 SB ：' + killedNum + '/' + len_curScan );
-		console.log('用户反馈：http://rrurl.cn/hM9mhk');
-		console.log('执行时刻：' + getTime());
-		console.log('\n');
-	});
-}
-
-function _log( msg ){
-	console.log( 'rrsb:' + msg );
+function loger ( killedNum, curScanCount, pageTitle ) {
+	var body = document.querySelector('body'),
+		scanCountPage = body.getAttribute('rrsb_scancount')||0,
+		killedNumPage = body.getAttribute('rrsb_killednum')||0;
+	console.log(
+		'\n【人人2B过滤器\'s log】',
+		'\n' + pageTitle + '：'+ window.location.href,
+		'\n本次 SB ：' + killedNum + '/' + curScanCount ,
+		'\n本页 SB ：' + killedNumPage + '/' + scanCountPage,
+		'\n用户反馈：http://rrurl.cn/hM9mhk',
+		'\n执行时刻：' + getTime() + '\n'
+	);
+	if( SBCmt.length ){
+		console.table( SBCmt );
+		SBCmt.length = 0;
+	}
 }
 
 function getTime() {
@@ -240,8 +295,25 @@ function getTime() {
 	return str;
 }
 
-function trim( str ) {
-	 return str.replace( /(^\s*)|(\s*$)/g , "" );
+function getDay(){
+	var d = new Date(),
+	y = d.getFullYear(),
+	m = d.getMonth()+1,
+	da = d.getDate();
+	return y + '-' + m + '-' + da;
+}
+
+function getId(){
+	var index = document.querySelector('#navBar .nav-main .menu-title a');
+	if( index.length ){
+		href = index.href;
+		var id = href.replace(/http:\/\/.*\..*\.com\//,'');
+		if( id.indexOf('id') > -1 ){
+			id = id.replace(/home\?id=/,'');
+		}
+		return id;
+	}
+	return '';
 }
 
 function getChinese( str ) {
